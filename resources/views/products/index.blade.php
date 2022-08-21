b7751a46db43e9274b64d40c85bbea0066c5e1fc
afta <hattpri.yhukimura@gmail.com>
2022-08-21 21:15:32 +0700

    revisi readme.txt

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,11 +4,6 @@
 <!-- <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script> -->
 <link rel="stylesheet" href="{{ asset('assets/sweetalert2/sweetalert2.css') }}">
 <script src="{{ asset('assets/sweetalert2/sweetalert2.js') }}"></script>
-
-<link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
-
-<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-
 <div class="container">
     <div class="row">
         <h2 class="page-header text-center">Table Product</h2>
